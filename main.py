310beb31272458ce5b3c1600c2e40982e789d7e3
Samarth Kalanke <samarthkalanke@gmail.com>
2023-06-04 16:32:47 -0700

    fsfsf

--- main.py
+++ main.py
@@ -39,15 +39,14 @@ def index():
     if request.method == "Post":
         db.add(users(username="Samarth",score=request.form["score"]))
         db.session.commit()
+    else:
+        return render_template("index.md")
 
-
-    return render_template("index.html")
 
 
 
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
-    from flask_cors import CORS
-    cors = CORS(app)
     app.run(debug=True, host="0.0.0.0", port="8086")
+
